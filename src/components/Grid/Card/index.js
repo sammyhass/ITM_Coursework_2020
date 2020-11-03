6238439930bd9a0bd34bf6fe1cca47d278edf3a4
Sammy Hass <sammyhass01@gmail.com>
2020-11-03 21:24:17 +0000

    Updated cards on hobby page for dark mode

--- src/components/Grid/Card/index.js
+++ src/components/Grid/Card/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import useDarkMode from 'use-dark-mode';
 
 import styles from './Card.module.css';
 
 const HobbyCard = ({ name, description, image, imageCredit }) => {
+	const darkMode = useDarkMode();
+	const [boxShadow, setBoxShadow] = useState('2px 2px 5px #eee');
+	useEffect(() => {
+		setBoxShadow(darkMode.value ? 'none' : '2px 2px 5px #eee');
+	}, [darkMode.value]);
 	return (
-		<div className={styles.HobbyCard}>
+		<div className={styles.HobbyCard} style={{ boxShadow: boxShadow }}>
 			<img src={image} alt={description} />
 
 			<h2>{name}</h2>
