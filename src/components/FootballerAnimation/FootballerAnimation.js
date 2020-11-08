836d13bbcff7b874e13b1c17b2b6e3d00b221c4c
Sammy Hass <sammyhass01@gmail.com>
2020-11-08 23:34:56 +0000

    Created animation of footballer

---
+++ src/components/FootballerAnimation/FootballerAnimation.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react';
+
+import Raphael from 'raphael';
+
+import footballImage from '../../assets/animation/Ball.png';
+import footballerRun from '../../assets/animation/Footballer.gif';
+import footballerKick from '../../assets/animation/Kicking.gif';
+import goalImage from '../../assets/animation/Goal.png';
+import useDarkMode from 'use-dark-mode';
+
+const FootballerAnimation = () => {
+	const darkMode = useDarkMode();
+
+	const animation = () => {
+		let paper = Raphael('animation_container', 800, 600);
+		let background = paper.rect(0, 0, 800, 600);
+		background.attr('fill', darkMode.value ? 'midnightblue' : 'skyblue');
+
+		let grass = paper.rect(0, 400, 800, 200);
+		grass.attr('fill', 'green');
+
+		let football = paper.image(footballImage, 210, 400, 37, 37);
+		football.hide();
+
+		let goal = paper.image(goalImage, 450, 275, 400, 300);
+		goal.toBack();
+		grass.toBack();
+		background.toBack();
+
+		let runningPlayer = paper.image(footballerRun, -200, 300, 200, 180);
+		let kickingPlayer = paper.image(footballerKick, 50, 300, 200, 180);
+		kickingPlayer.hide();
+
+		const runForward = () => {
+			football.animate({ x: 210, y: 400 });
+			football.hide();
+			runningPlayer.animate({ x: 100 }, 1000, 'ease-in', kick);
+		};
+
+		const kick = () => {
+			runningPlayer.hide();
+			kickingPlayer.show();
+			setTimeout(() => {
+				football.show();
+			}, 280);
+
+			football.animate({ x: 700, y: 350 }, 1000, 'linear', resetBall);
+		};
+
+		const resetBall = () => {
+			kickingPlayer.remove();
+			runningPlayer = paper.image(footballerRun, -200, 300, 200, 180);
+			runningPlayer.animate({ x: -200 }, 100, 'bounce');
+
+			kickingPlayer = paper.image(footballerKick, 50, 300, 200, 180);
+			kickingPlayer.hide();
+			runningPlayer.show();
+			football.animate({ x: -210, y: 400 }, 1000, runForward);
+		};
+
+		setTimeout(runForward, 1000);
+
+		return () => {
+			paper.clear();
+		};
+	};
+
+	useEffect(animation, [darkMode.value]);
+
+	return <div id="animation_container"></div>;
+};
+
+export default FootballerAnimation;
--- src/pages/Animation.js
+++ src/pages/Animation.js
@@ -1,35 +1,12 @@
-import React, { useEffect } from 'react';
-import Raphael from 'raphael';
+import React from 'react';
 
+import FootballerAnimation from '../components/FootballerAnimation/FootballerAnimation';
+
+import styles from './Animation.module.css';
 const Animation = () => {
-	const animation = () => {
-		let paper = Raphael('animate_container', 800, 600);
-		let background = paper.rect(0, 0, 800, 600);
-		background.attr({ fill: 'blue' });
-
-		let ball = paper.circle(50, 50, 30);
-		ball.attr({ fill: '45-green-yellow' });
-
-		const bounceDrop1 = () => {
-			ball.animate({ cy: 570, cx: 400 }, 500, 'ease-in', bounceUp1);
-		};
-		const bounceUp1 = () => {
-			ball.animate({ cy: 50, cx: 750 }, 500, 'ease-out', bounceDrop2);
-		};
-		const bounceDrop2 = () => {
-			ball.animate({ cy: 570, cx: 400 }, 500, 'ease-in', bounceUp2);
-		};
-		const bounceUp2 = () => {
-			ball.animate({ cy: 50, cx: 50 }, 500, 'ease-out', bounceDrop1);
-		};
-		bounceDrop1();
-	};
-
-	useEffect(animation);
-
 	return (
-		<div>
-			<div id="animate_container"></div>
+		<div className={styles.Animation}>
+			<FootballerAnimation />
 		</div>
 	);
 };
