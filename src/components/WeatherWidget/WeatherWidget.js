ea13d14dea90e2c67d2729da89fafe9e5bc384e5
Sammy Hass <sammyhass01@gmail.com>
2020-11-19 01:39:19 +0000

    Updated url for icon so it works on all devices

--- src/components/WeatherWidget/WeatherWidget.js
+++ src/components/WeatherWidget/WeatherWidget.js
@@ -48,7 +48,7 @@ const WeatherWidget = () => {
 			<div>
 				<img
 					src={
-						'http://openweathermap.org/img/wn/' +
+						'https://openweathermap.org/img/wn/' +
 						weatherData.weather[0].icon +
 						'@2x.png'
 					}
